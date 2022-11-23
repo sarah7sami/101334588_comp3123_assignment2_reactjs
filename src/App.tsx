1b52687bb21c07bf45dcb555a385aafafe593bf3
Sarah Sami <sarah@Sarahs-MacBook-Pro.local>
2022-11-22 20:11:37 -0500

    added react-router, testing material UI theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React from "react";
+import {
+  Box,
+  CssBaseline,
+  Paper,
+  ThemeProvider,
+  Typography,
+} from "@mui/material";
+import { createTheme } from "@mui/material/styles";
 
 function App() {
-  const [count, setCount] = useState(0)
+    
+  // define theme
+  const theme = createTheme({
+    palette: {
+      primary: {
+        light: "#63b8ff",
+        main: "#0989e3",
+        dark: "#005db0",
+        contrastText: "#000",
+      },
+      secondary: {
+        main: "#4db6ac",
+        light: "#82e9de",
+        dark: "#00867d",
+        contrastText: "#000",
+      },
+    },
+  });
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Box
+        height="100vh"
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        flexDirection="column"
+      >
+        <Paper
+          elevation={3}
+          sx={{ padding: "1rem", backgroundColor: "secondary.light" }}
+        >
+          <Typography color="primary.dark" variant="h1">
+            Starter App
+          </Typography>
+        </Paper>
+      </Box>
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
